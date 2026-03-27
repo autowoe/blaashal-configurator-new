bd9b63ae48efda08b77c1e8cadbbdbb5c6b171b6
Lex Wuestenenk <lex.wuestenenk@hotmail.com>
2026-03-28 00:32:23 +0100

    fix: gunicorn config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
